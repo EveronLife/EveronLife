e4876bd79a64e08e2bce85af9f2664cbe134e867
MaddieVR <matt2a.wright@gmail.com>
2022-05-30 19:43:01 -0400

    Beta HUD
    Updated Beta HUD to have, Health, Stamina, Thirst, Hunger, and Money.
    
    Health and Stamina are currently functional.

--- src/Scripts/Game/UI/EL_BetaHud.c
+++ src/Scripts/Game/UI/EL_BetaHud.c
@@ -1,3 +1,96 @@
 class EL_BetaHud : SCR_InfoDisplay
 {
+	private SliderWidget staminaSlider = null;
+	private SliderWidget healthSlider = null;
+	private SliderWidget thirstSlider = null;
+	private SliderWidget hungerSlider = null;
+	private SCR_CharacterControllerComponent playerController = null;
+	private DamageManagerComponent DMC = null;
+	
+	//--------------------------- OnChangeFunctions --------------------------- 
+	void OnHealthChange(float value)
+	{
+		if (!healthSlider)
+		{
+			healthSlider = SliderWidget.Cast(m_wRoot.FindWidget("healthSlider"));
+			if (!healthSlider) return;
+		};
+		
+		healthSlider.SetCurrent(value)
+	}
+	
+	void OnStaminaChange(float value)
+	{
+		if (!staminaSlider)
+		{
+			staminaSlider = SliderWidget.Cast(m_wRoot.FindWidget("staminaSlider"));
+			if (!staminaSlider) return;
+		};
+		
+		staminaSlider.SetCurrent(value);
+	}
+	
+	void OnThirstChange(float value)
+	{
+		if (!thirstSlider)
+		{
+			thirstSlider = SliderWidget.Cast(m_wRoot.FindWidget("thirstSlider"));
+			if (!thirstSlider) return;
+		};
+		
+		thirstSlider.SetCurrent(value);
+	}
+	
+	void OnHungertChange(float value)
+	{
+		if (!hungerSlider)
+		{
+			hungerSlider = SliderWidget.Cast(m_wRoot.FindWidget("hungerSlider"));
+			if (!hungerSlider) return;
+		};
+		
+		hungerSlider.SetCurrent(value);
+	}
+	
+	//--------------------------- Main Functions --------------------------- 
+	override event void OnStartDraw(IEntity owner)
+	{
+		super.OnStartDraw(owner);
+
+		staminaSlider = SliderWidget.Cast(m_wRoot.FindWidget("staminaSlider"));
+		healthSlider = SliderWidget.Cast(m_wRoot.FindWidget("healthSlider"));
+		IEntity player = GetGame().GetPlayerController();
+		if (!player) {
+			Print("no player found");
+			return;
+		}
+		
+		playerController = SCR_CharacterControllerComponent.Cast(player.FindComponent(SCR_CharacterControllerComponent));
+		if (playerController) Print("Found character controller component")
+	}
+	
+	
+	//UpdateValues needs to be called upon respawning as to reset the UI -- KNOWN BUG
+	override event void UpdateValues(IEntity owner, float timeSlice)
+	{
+		if (!playerController)
+		{
+			IEntity player = SCR_PlayerController.GetLocalControlledEntity();
+			if (!player) {
+				return;
+			}
+		
+			playerController = SCR_CharacterControllerComponent.Cast(player.FindComponent(SCR_CharacterControllerComponent));
+			if (!playerController) return;
+			
+			DMC = DamageManagerComponent.Cast(player.FindComponent(DamageManagerComponent));
+			if (!DMC) return;
+			
+			//add lines to get thirst and hunger here
+			
+		};
+		
+		OnStaminaChange(playerController.GetStamina());
+		OnHealthChange(DMC.GetHealth())
+	}
 }
