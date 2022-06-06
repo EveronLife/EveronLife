b94102e0fa683663cfe8d93663571712dcc5563b
Jacob <mangosprograms@gmail.com>
2022-06-06 21:57:58 +1000

    Quantity size text truncation in slot UI

--- src/Scripts/Game/UI/InventorySystem/EL_InventorySlotUI.c
+++ src/Scripts/Game/UI/InventorySystem/EL_InventorySlotUI.c
@@ -22,4 +22,39 @@ modded class SCR_InventorySlotUI
 	{
 		return m_EL_QuantityComponent;
 	}
+	
+	override void UpdateStackNumber()
+	{
+		super.UpdateStackNumber();
+
+		if (!m_EL_QuantityComponent || !m_wStackNumber)
+		{
+			return;
+		}
+
+		array<ref Tuple2<int, string>> numerals();
+		numerals.Insert(new Tuple2<int, string>(0, ""));
+		numerals.Insert(new Tuple2<int, string>(1000, "K"));
+		numerals.Insert(new Tuple2<int, string>(1000000, "M"));
+		numerals.Insert(new Tuple2<int, string>(1000000000, "B"));
+
+		for (int i = 0; i < numerals.Count(); i++)
+		{
+			if (m_iStackNumber > numerals[i].param1)
+			{
+				continue;
+			}
+
+			auto numeral = numerals[i - 1];
+			int divisor = numeral.param1;
+			int quantity = m_iStackNumber;
+			if (divisor != 0)
+			{
+				quantity /= divisor;
+			}
+
+			m_wStackNumber.SetText(quantity.ToString() + numeral.param2);
+			break;
+		}
+	}
 };
