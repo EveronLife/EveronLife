4bda50514bdab4c54408d73d9f59411184b96bc6
Gabriel <aguiargab@gmail.com>
2022-06-09 22:55:19 -0300

    Revert "Change license plate"
    This reverts commit fc1e99836b25797e6f60c9a1e912f752a40d60b9.

--- src/Scripts/Game/Features/LicensePlate/EL_LicensePlateGeneratorGeneric.c
+++ src/Scripts/Game/Features/LicensePlate/EL_LicensePlateGeneratorGeneric.c
@@ -5,8 +5,9 @@ class EL_LicensePlateGeneratorGeneric: EL_LicensePlateGeneratorBase
 	{
 		string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 		string p1 = alphabet.Get(Math.RandomInt(0, 25)) + alphabet.Get(Math.RandomInt(0, 25));
-		string p2 = Math.RandomInt(100, 9999).ToString();
+		string p2 = alphabet.Get(Math.RandomInt(0, 25)) + alphabet.Get(Math.RandomInt(0, 25));
+		string p3 = Math.RandomInt(100, 9999).ToString();
 		
-		return string.Format("%1 %2", p1, p2);
+		return string.Format("%1 %2 %3", p1, p2, p3);
 	}
 }
