cb0a72c585a22e8f429dbfeb66306ac0d3db4776
TheBonBon <45827986+thebonbon@users.noreply.github.com>
2022-06-21 11:53:27 +0200

    Remove wrong comments

--- src/Scripts/Game/World/EL_ChangeTimeAction.c
+++ src/Scripts/Game/World/EL_ChangeTimeAction.c
@@ -3,10 +3,7 @@ class EL_ChangeTimeAction : ScriptedUserAction
 	[Attribute(defvalue:"12", UIWidgets.Slider, desc: "Target Time", "0 23 1")]
 	private int m_fTargetTime;
 
-	
 	//------------------------------------------------------------------------------------------------
-	// User has performed the action
-	// play a pickup sound and then add the correct amount to the users inventory
 	override void PerformAction(IEntity pOwnerEntity, IEntity pUserEntity)
 	{
 		TimeAndWeatherManagerEntity weatherManager = GetGame().GetTimeAndWeatherManager();
@@ -17,8 +14,6 @@ class EL_ChangeTimeAction : ScriptedUserAction
 	}
 	
 	//------------------------------------------------------------------------------------------------
-	// Checks if a required item has been set in the Editor
-	// If so, check if its in the users inventory/hands depending on settings set
 	override bool CanBePerformedScript(IEntity user)
  	{
 		return true;
--- src/Scripts/Game/World/EL_ChangeWeatherAction.c
+++ src/Scripts/Game/World/EL_ChangeWeatherAction.c
@@ -3,10 +3,7 @@ class EL_ChangeWeatherAction : ScriptedUserAction
 	[Attribute(defvalue:"", UIWidgets.Slider, desc: "Weather ID", "0 3 1")]
 	private int m_fTargetWeatherID;
 
-	
 	//------------------------------------------------------------------------------------------------
-	// User has performed the action
-	// play a pickup sound and then add the correct amount to the users inventory
 	override void PerformAction(IEntity pOwnerEntity, IEntity pUserEntity)
 	{
 		TimeAndWeatherManagerEntity weatherManager = GetGame().GetTimeAndWeatherManager();
@@ -26,8 +23,6 @@ class EL_ChangeWeatherAction : ScriptedUserAction
 	}
 	
 	//------------------------------------------------------------------------------------------------
-	// Checks if a required item has been set in the Editor
-	// If so, check if its in the users inventory/hands depending on settings set
 	override bool CanBePerformedScript(IEntity user)
  	{
 		return true;
