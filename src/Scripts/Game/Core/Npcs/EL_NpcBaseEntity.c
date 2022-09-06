a67b854f6d483c32697d4ec493e1b1475dfdb92d
Arkensor <arkensor@gmail.com>
2022-09-06 12:06:25 +0200

    Add npc base class/prefab (#157)
    Co-authored-by: thebonbon <45827986+thebonbon@users.noreply.github.com>

---
+++ src/Scripts/Game/Core/Npcs/EL_NpcBaseEntity.c
@@ -0,0 +1,8 @@
+[EntityEditorProps(category: "EveronLife/Core/Npcs", description:"General purpose NPC entity")]
+class EL_NpcBaseEntityClass : GenericEntityClass
+{
+}
+
+class EL_NpcBaseEntity : GenericEntity
+{
+}
