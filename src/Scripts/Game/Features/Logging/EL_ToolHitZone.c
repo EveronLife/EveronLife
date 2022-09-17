b36a986a5e60cb72a15ad7384b2db2da8d1213a4
TheBonBon <45827986+thebonbon@users.noreply.github.com>
2022-09-17 12:06:02 +0200

    Fix tool hit zone

--- src/Scripts/Game/Features/Logging/EL_ToolHitZone.c
+++ src/Scripts/Game/Features/Logging/EL_ToolHitZone.c
@@ -3,31 +3,27 @@ class EL_ToolHitZone : ScriptedHitZone
 
 	[Attribute("", UIWidgets.ResourceNamePicker, "Particle to spawn on hit", "et")]
 	ResourceName m_HitVFX;
-	[Attribute("", UIWidgets.EditBox, "Tool to enable dmg eg 'ToolAxe'")]
-	string m_sToolName;
-
-	DamageManagerComponent m_DamageManager;
-	bool m_bAddedDamage;
-
-
+	[Attribute("", UIWidgets.Slider, "Damage reference to the effective tool", "0.0 100.0 1.0")]
+	float m_fEffectiveDamage;
 
 	//------------------------------------------------------------------------------------------------
 	override void OnDamage(EDamageType type, float damage, HitZone pOriginalHitzone, IEntity instigator, inout vector hitTransform[3], float speed, int colliderID, int nodeID)
 	{
-		//Axe check
-		if (damage != 20.22)
-			return;
-
 		EL_Utils.SpawnEntityPrefab(m_HitVFX, hitTransform[0], hitTransform[1]);
-
-		HandleDamage(damage * 2, EDamageType.MELEE, instigator);
+		
+		if (!instigator) return;
+		
+		//Heal any damage that is not the effective tool (dmg)
+		if (damage != m_fEffectiveDamage)
+			HandleDamage(-damage, EDamageType.TRUE, instigator);
+		
+		
 	}
-	
+
 	//------------------------------------------------------------------------------------------------
 	override void OnInit(IEntity pOwnerEntity, GenericComponent pManagerComponent)
 	{
 		super.OnInit(pOwnerEntity, pManagerComponent);
-		m_DamageManager = DamageManagerComponent.Cast(pOwnerEntity.FindComponent(DamageManagerComponent));
 	}
 
 };
