afe8e9cd403b4ef482005247fdc490e7dc480875
TheBonBon <45827986+thebonbon@users.noreply.github.com>
2022-09-20 19:42:51 +0200

    Add tool effective damage

--- src/Scripts/Game/Features/Logging/EL_ToolHitZone.c
+++ src/Scripts/Game/Features/Logging/EL_ToolHitZone.c
@@ -3,9 +3,16 @@ class EL_ToolHitZone : ScriptedHitZone
 
 	[Attribute("", UIWidgets.ResourceNamePicker, "Particle to spawn on hit", "et")]
 	ResourceName m_HitVFX;
-	[Attribute("", UIWidgets.Slider, "Damage reference to the effective tool", "0.0 100.0 1.0")]
+	[Attribute("", UIWidgets.Slider, "Effective tool damage", "0.0 100.0 0.01")]
 	float m_fEffectiveDamage;
 
+	//------------------------------------------------------------------------------------------------
+	override void OnDamage(EDamageType type, float damage, HitZone pOriginalHitzone, IEntity instigator, inout vector hitTransform[3], float speed, int colliderID, int nodeID)
+	{
+		//Melee damage reduction is 0.01
+		if (damage == m_fEffectiveDamage * 0.01)
+			HandleDamage(damage * 100, EDamageType.TRUE, instigator);
+	}
 	//------------------------------------------------------------------------------------------------
 	override void OnLocalDamage(EDamageType type, float damage, HitZone pOriginalHitzone, IEntity damageSource, IEntity instigator, inout vector hitTransform[3], float speed, int colliderID, int nodeID)
 	{
