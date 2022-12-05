6411c5f4944877e289d696f8d778371b1b57fdb2
TheBonBon <45827986+thebonbon@users.noreply.github.com>
2022-12-05 16:33:07 +0100

    Merge Money Utils

--- src/Scripts/Game/Feature/Money/EL_MoneyUtils.c
+++ src/Scripts/Game/Feature/Money/EL_MoneyUtils.c
@@ -3,6 +3,71 @@ class EL_MoneyUtils
 	static const ResourceName PREFAB_CASH = "{5439738849229352}Prefabs/Items/Currencies/MoneyStack.et";
 
 	//------------------------------------------------------------------------------------------------
+	static bool CanBuy(notnull IEntity player, ResourceName prefabToBuy, int price, out string errorMessage)
+	{
+		SCR_InventoryStorageManagerComponent inventoryManager = SCR_InventoryStorageManagerComponent.Cast(player.FindComponent(SCR_InventoryStorageManagerComponent));
+		if (!inventoryManager)
+			return false;
+
+		if (EL_MoneyUtils.GetCash(inventoryManager) < price)
+		{
+			errorMessage = "Can't afford";
+			return false;
+		}
+		
+		if (!EL_InventoryUtils.CanSpawnPrefabToStorage(inventoryManager, prefabToBuy))
+		{
+			errorMessage = "Inventory full";
+			return false;
+		}
+		
+		return true;
+	}
+	
+	//------------------------------------------------------------------------------------------------
+	static bool TryBuy(notnull IEntity player, ResourceName prefabToBuy, int price)
+	{
+		SCR_InventoryStorageManagerComponent inventoryManager = SCR_InventoryStorageManagerComponent.Cast(player.FindComponent(SCR_InventoryStorageManagerComponent));
+		if (!inventoryManager)
+			return false;
+
+		if (EL_MoneyUtils.GetCash(inventoryManager) < price)
+			return false;
+		
+		//Spawn bought Item
+		bool insertSucess = inventoryManager.TrySpawnPrefabToStorage(prefabToBuy);
+		if (!insertSucess)
+			return false;
+
+		RemoveCash(player, price);
+		
+		return true;
+	}
+
+	//------------------------------------------------------------------------------------------------
+	static bool TrySell(notnull IEntity player, ResourceName prefabToSell, int price)
+	{
+		if (prefabToSell.IsEmpty())
+			return false;
+		SCR_PrefabNamePredicate prefabNamePredicate();
+		prefabNamePredicate.prefabName = prefabToSell;
+
+		SCR_InventoryStorageManagerComponent inventoryManager = SCR_InventoryStorageManagerComponent.Cast(player.FindComponent(SCR_InventoryStorageManagerComponent));
+		if (!inventoryManager)
+			return false;
+
+		//Delete sold Item
+		bool deleteSucess = inventoryManager.TryDeleteItem(inventoryManager.FindItem(prefabNamePredicate));
+		if (!deleteSucess)
+			return false;
+
+		//Spawn Money
+		AddCash(player, price);
+
+		return true;
+	}
+	
+	//------------------------------------------------------------------------------------------------
 	//! Get the total amount of cash the target has across all storages
 	//! \return the total cash or -1 on faulty opertations.
 	static int GetCash(InventoryStorageManagerComponent target)
