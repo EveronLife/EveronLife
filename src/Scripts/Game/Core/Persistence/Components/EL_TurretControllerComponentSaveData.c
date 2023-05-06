1ddaf6ef26f1d8ecb2f2813012a018568ddc4891
Arkensor <arkensor@gmail.com>
2023-05-06 17:42:00 +0200

    Add current turret active idx to save-data

--- src/Scripts/Game/Core/Persistence/Components/EL_TurretControllerComponentSaveData.c
+++ src/Scripts/Game/Core/Persistence/Components/EL_TurretControllerComponentSaveData.c
@@ -8,11 +8,17 @@ class EL_TurretControllerComponentSaveData : EL_ComponentSaveData
 {
 	float m_fYaw;
 	float m_fPitch;
+	int m_iSelectedWeaponSlotIdx;
 
 	//------------------------------------------------------------------------------------------------
 	override EL_EReadResult ReadFrom(IEntity owner, GenericComponent component, EL_ComponentSaveDataClass attributes)
 	{
 		TurretControllerComponent turretController = TurretControllerComponent.Cast(component);
+
+		BaseWeaponManagerComponent weaponManager = turretController.GetWeaponManager();
+		WeaponSlotComponent currentSlot = weaponManager.GetCurrentSlot();
+		if (currentSlot)
+			m_iSelectedWeaponSlotIdx = currentSlot.GetWeaponSlotIndex();
 
 		TurretComponent turret = turretController.GetTurretComponent();
 		if (!turret)
@@ -40,8 +46,23 @@ class EL_TurretControllerComponentSaveData : EL_ComponentSaveData
 			}
 		}
 
-		// Check if there is any diff to default angles
-		if (vector.Distance(angles, initAiming) <= 0.0001)
+		int defaultIdx;
+		BaseContainer weaponManagerSource = weaponManager.GetComponentSource(owner);
+		weaponManagerSource.Get("DefaultWeaponIndex", defaultIdx);
+		if (defaultIdx == -1)
+		{
+			WeaponSlotComponent firstSlot = EL_Component<WeaponSlotComponent>.Find(owner);
+			if (firstSlot)
+			{
+				defaultIdx = firstSlot.GetWeaponSlotIndex();
+			}
+			else
+			{
+				defaultIdx = 0;
+			}
+		}
+
+		if (vector.Distance(angles, initAiming) <= 0.0001 && (m_iSelectedWeaponSlotIdx == defaultIdx))
 			return EL_EReadResult.DEFAULT;
 
 		return EL_EReadResult.OK;
@@ -52,6 +73,27 @@ class EL_TurretControllerComponentSaveData : EL_ComponentSaveData
 	{
 		TurretControllerComponent turretController = TurretControllerComponent.Cast(component);
 		turretController.SetAimingAngles(m_fYaw * Math.DEG2RAD, m_fPitch * Math.DEG2RAD);
+
+		/*
+		TODO: Wait for https://feedback.bistudio.com/T172197 to be added so we can select the weapon from the manager
+
+		BaseWeaponManagerComponent weaponManager = turretController.GetWeaponManager();
+		WeaponSlotComponent currentSlot = weaponManager.GetCurrentSlot();
+		if (!currentSlot || currentSlot.GetWeaponSlotIndex() != m_iSelectedWeaponSlotIdx)
+		{
+			array<WeaponSlotComponent> outSlots();
+			turretController.GetWeaponManager().GetWeaponsSlots(outSlots);
+			foreach (WeaponSlotComponent weaponSlot : outSlots)
+			{
+				if (weaponSlot.GetWeaponSlotIndex() == m_iSelectedWeaponSlotIdx)
+				{
+					weaponManager.SelectWeapon(weaponSlot);
+					break;
+				}
+			}
+		}
+		*/
+
 		return EL_EApplyResult.OK;
 	}
 
@@ -59,6 +101,8 @@ class EL_TurretControllerComponentSaveData : EL_ComponentSaveData
 	override bool Equals(notnull EL_ComponentSaveData other)
 	{
 		EL_TurretControllerComponentSaveData otherData = EL_TurretControllerComponentSaveData.Cast(other);
-		return float.AlmostEqual(m_fYaw, otherData.m_fYaw) && float.AlmostEqual(m_fPitch, otherData.m_fPitch);
+		return float.AlmostEqual(m_fYaw, otherData.m_fYaw) &&
+			float.AlmostEqual(m_fPitch, otherData.m_fPitch) &&
+			m_iSelectedWeaponSlotIdx == otherData.m_iSelectedWeaponSlotIdx;
 	}
 };
